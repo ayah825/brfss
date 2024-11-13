804b52f9768b956ed5fed2fdf02362acf747f02b
ayah825 <156377548+ayah825@users.noreply.github.com>
2024-11-12 19:03:17 -0800

    added code

--- sas2csv.R
+++ sas2csv.R
@@ -1,17 +1,5 @@
-#options(stringsAsFactors=F, scipen = 999)
-
-#pkg = "Hmisc"
-#if (!require(pkg, character.only = TRUE)) {
-#  install.packages(pkg)
-#  library(pkg, character.only = TRUE)
-#}
-
-
-#brfss <- sasxport.get("BRFSS_2023.XPT")
-#write.csv(brfss, file = "brfss2023.csv")
-
 # change the file BRFSS_2023.XPT to csv
 setwd("/Users/ayahibrahim/Documents/GitHub")
 library(haven)
-brfss <- read_xpt('BRFSS_2023.XPT')
-write.csv(brfss, file = "brfss2023.csv")
+brfss <- read_xpt('BRFSS_2022.XPT')
+write.csv(brfss, file = "brfss2022.csv")
